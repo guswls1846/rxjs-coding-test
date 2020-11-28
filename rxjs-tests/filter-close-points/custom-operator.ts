007ae07658590e9f4a5a293cadca0579716a3b9f
nbbang <nbbang@MacBook-Pro.local>
2020-11-28 22:06:38 +0900

    custom-operator 완료

--- rxjs-tests/filter-close-points/custom-operator.ts
+++ rxjs-tests/filter-close-points/custom-operator.ts
@@ -1,8 +1,8 @@
 import { pipe, Observable, UnaryFunction } from "rxjs";
 import { Point } from "./lib/test-source";
+import { map, filter } from "rxjs/operators";
 
 export function filterClosePoints(distance: number): UnaryFunction<Observable<Point>, Observable<Point>> {
-  return pipe(
-    // TODO
-  );
+  return pipe();
+  // TODO
 }
--- rxjs-tests/grouping/custom-operator.ts
+++ rxjs-tests/grouping/custom-operator.ts
@@ -1,7 +1,15 @@
 import { pipe, UnaryFunction, Observable } from "rxjs";
-
+import { filter, groupBy, mergeMap, toArray, map, tap } from "rxjs/operators";
 export function sortByTitle(gender: string): UnaryFunction<Observable<any>, Observable<string[]>> {
   return pipe(
     // TODO
+    filter((data) => data.gender === gender),
+    groupBy((person) => person.title),
+    mergeMap((group) =>
+      group.pipe(
+        map((data) => data.name),
+        toArray()
+      )
+    )
   );
 }
