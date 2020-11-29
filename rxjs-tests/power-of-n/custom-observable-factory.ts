ea07d6d82434df3b178724647ca69ebb630cf242
nbbang <nbbang@MacBook-Pro.local>
2020-11-29 16:45:07 +0900

    power-of-n 완료

--- rxjs-tests/power-of-n/custom-observable-factory.ts
+++ rxjs-tests/power-of-n/custom-observable-factory.ts
@@ -1,5 +1,9 @@
-import { Observable } from "rxjs";
+import { Observable, interval, from, of, concat } from "rxjs";
+import { map, concatMap, delay, mergeMap, switchMap, takeUntil, takeWhile } from "rxjs/operators";
 
 export function output$(input: number[]): Observable<number> {
   // TODO
+  const squareRoot = (val) => interval(1000).pipe(map((i) => Math.pow(val, i + 1)));
+  const source = from(input).pipe(concatMap((val) => (val !== 1 ? squareRoot(val).pipe(takeWhile((val) => val < 5000)) : of(val))));
+  return source;
 }
