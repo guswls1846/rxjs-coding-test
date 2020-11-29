cbfea18f4977c6df9b870f439954dbcc63179b32
nbbang <nbbang@MacBook-Pro.local>
2020-11-29 21:40:15 +0900

    custom-operator 완료..

--- rxjs-tests/retry-interval-timeout/index.ts
+++ rxjs-tests/retry-interval-timeout/index.ts
@@ -2,15 +2,17 @@ import { getRandomNumber, interval, timeout } from "./lib/test-sources";
 import { retryIntervalTimeout } from "./custom-operator";
 
 const startTime = Date.now();
-export const output$ = getRandomNumber().pipe(
-  retryIntervalTimeout(interval, timeout)
-);
+export const output$ = getRandomNumber().pipe(retryIntervalTimeout(interval, timeout));
 
 // Caution: success or error should be emitted once!
-output$.subscribe(output => {
-  console.log('@@@ success: ', output, (Date.now() - startTime) / 1000); //
-}, error => {
-  console.log('@@@ error: ', error.message, (Date.now() - startTime) / 1000);
-}, () => {
-  console.log('@@@ complete', (Date.now() - startTime) / 1000);
-});
+output$.subscribe(
+  (output) => {
+    console.log("@@@ success: ", output, (Date.now() - startTime) / 1000); //
+  },
+  (error) => {
+    console.log("@@@ error: ", error.message, (Date.now() - startTime) / 1000);
+  },
+  () => {
+    console.log("@@@ complete", (Date.now() - startTime) / 1000);
+  }
+);
--- rxjs-tests/retry-interval-timeout/lib/test-sources.ts
+++ rxjs-tests/retry-interval-timeout/lib/test-sources.ts
@@ -1,17 +1,17 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 export function getRandomNumber(): Observable<number> {
-  console.log('getRandomNumber invoked!!');
-  return new Observable<number>(observer => {
+  console.log("getRandomNumber invoked!!");
+  return new Observable<number>((observer) => {
     let number = Math.random();
-    console.log('Observable.create!!', number);
+    console.log("Observable.create!!", number);
     if (number > 0.8) {
       observer.next(number);
       observer.complete();
     } else {
-      observer.error(new Error('The number is not big enough'));
+      observer.error(new Error("The number is not big enough"));
     }
-  })
+  });
 }
 
 export const interval = 300;
